a23c9e442a1003be79897f0f8160642e37a3fa94
Derrick Owusu <owusuderek@gmail.com>
2026-03-26 17:12:23 -0700

    index.js file updated with canEnterClub function

--- index.js
+++ index.js
@@ -165,6 +165,11 @@ export function makeSLS(str1, str2) {
  */
 export function canEnterClub(you, date) {
   /* TODO */
+  if (you <= 2 || date <= 2) {
+    return 0;
+  } else if (you >= 8 || date >= 8) {
+    return 2;
+  } else return 1;
 }
 
 /**
